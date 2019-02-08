16699719751ebefb53d85c41308ec12e06bb64ea
Błażej Lalowicz <blalowicz@opteam.local>
2019-02-08 14:09:45 +0100

    commit

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -76,8 +76,7 @@ namespace DatingApp.API.Controllers
             return Ok(
                 new {
                     token = tokenHandler.WriteToken(token)
-                }
-            );
+                });
         }
     }
 }
